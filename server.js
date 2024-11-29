c02fbc73db6777a43d9c335ced9eb2349a0dd0b2
Gdugboy <gdugboy@gmail.com>
2024-11-29 13:52:19 +0900

    cookie-session ok, server.js edit ok

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5500; // 프론트엔드 서버 포트
 
-// GET 요청 처리
-app.get('/', (req, res) => {
-    res.send('Get 요청 확인');
-});
+// FE 폴더를 정적 파일 경로로 설정
+app.use(express.static('FE'));
 
-// 서버 시작
+// 서버 실행
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`프론트 서버 시작 : http://localhost:${port}`);
 });
